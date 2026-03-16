1a83fbefce5ee1bd06c12c3f603a3c9da095ab75
shreekhushi <khushi.shree04@gmail.com>
2026-03-16 20:12:02 +0530

    switch-case examples

---
+++ Contact.java
@@ -0,0 +1,474 @@
+class Contact{
+  static long searchContactByName(String name){
+  
+  // long number = 00;
+  switch(name){
+	case "Mummy":
+	    System.out.println("case 1");
+	    return 9845803407L;
+	  
+	case "Pappa":
+	    System.out.println("case 2");
+	    return 7760289815L;
+	  
+	  
+    case "Vivek Bruvv":
+	    System.out.println("case 3");
+	    return 8951505885L;
+	  
+	  
+	case "Mishii":
+	    System.out.println("case 4");
+	    return 9036315166L;
+	  
+	  
+	case "Dummo":
+	    System.out.println("case 5");
+	    return 8951344162L;
+	  
+	case "Moni":
+	    System.out.println("case 6");
+	    return 9110818086L;
+	  
+	  
+	case "Shreyyy":
+	    System.out.println("case 7");
+	    return 8217216186L;
+	 
+	  
+	case "Jeevan Bruh":
+	    System.out.println("case 8");
+	    return 9380174013L;
+	  
+	case "Varshi":
+	    System.out.println("case 9");
+	    return 9036883139L;
+
+	case "Abnormal":
+	    System.out.println("case 10");
+	    return 8431630905L;
+	  
+	case "Adi":
+	    return 7353932201L;
+	  
+	case "Akkaa":
+	    return 9008412533L;
+	  
+	case "Akshara Sir":
+        return 9901191487L;
+
+    case "Amrutha":
+        return 7204978492L;
+
+    case "Ananya Nuchin":
+        return 7204730296L;
+
+    case "Anu akka":
+        return 8296325691L;
+
+    case "Arun Sir":
+        return 9945221208L;
+
+    case "Asfiya Senio":
+        return 6360961126L;
+
+    case "Asha Akka":
+        return 7975473454L;
+
+    case "Dr.Avinash":
+        return 9591255611L;
+
+    case "Banashree":
+        return 7892467943L;
+
+    case "Barath Yashu":
+        return 8296592494L;
+
+    case "Dr Batra's":
+        return 8291576277L;
+
+    case "Belli":
+        return 8310287354L;
+
+    case "Bhooma Senio":
+        return 8073780583L;
+
+    case "Bhoomika Mba":
+        return 7892184458L;
+
+    case "Bhumi Kal":
+        return 8050860849L;
+
+    case "Biet Xerox":
+        return 9743590139L;
+
+    case "Bnglr Xerox":
+        return 7259712357L;
+
+    case "Brundha":
+        return 9740945814L;
+
+    case "Chaitra CS Gmit":
+        return 9353076641L;
+
+    case "Chandana Xworkz":
+        return 8867739484L;
+
+    case "Chanduu":
+        return 9113549202L;
+
+    case "Chikkappa":
+        return 6360667823L;
+
+    case "Chinmay Akka":
+        return 7975325235L;
+
+    case "Choreographer":
+        return 9738315888L;
+
+    case "Croma":
+        return 7019326633L;
+
+    case "Darshan A Sec":
+        return 8431416159L;
+
+    case "Darshan Blore":
+        return 8197846236L;
+
+    case "Deepu Kan":
+        return 8296375779L;
+
+    case "Devu":
+        return 8606365498L;
+
+    case "Disha A Sec":
+        return 8095125669L;
+
+    case "Dodamma":
+        return 6363211489L;
+
+    case "Dominos":
+        return 7760631297L;
+
+    case "Ekadantha":
+        return 9844933734L;
+
+    case "Eshwari Bgscet":
+        return 7483551634L;
+
+    case "Ganesh Sir":
+        return 9845446315L;
+
+    case "Gmit Meghana A":
+        return 8618635626L;
+
+    case "Gubbi":
+        return 9380826489L;
+
+    case "Gubbi Pappa":
+        return 6363564614L;
+
+    case "Hamika":
+        return 7795346499L;
+
+    case "Harish Wifi":
+        return 6366992814L;
+
+    case "Harsha Cs":
+        return 9353241569L;
+
+    case "Harshi":
+        return 8073079490L;
+
+    case "Harshitha Cs":
+        return 9110208594L;
+
+    case "Harshitha Senio":
+        return 8123657881L;
+
+    case "Hod":
+        return 9448109123L;
+
+    case "HR":
+        return 7676749324L;
+
+    case "Imran Sir":
+        return 8073109504L;
+
+    case "Java Sir":
+        return 8496866392L;
+
+    case "Jeev":
+        return 9742570530L;
+
+    case "Jeev Vivekk":
+        return 7892052624L;
+
+    case "Jeevan":
+        return 8496088530L;
+
+    case "Kala":
+        return 9916364295L;
+
+    case "Kalpi":
+        return 7892777170L;
+
+    case "Kanika Junio":
+        return 7892185114L;
+
+    case "Kavana Akka":
+        return 7348954154L;
+
+    case "Kavana Gmit":
+        return 8660643320L;
+
+    case "Kavya Mam":
+        return 8123506523L;
+
+    case "Kavya N C Mam":
+        return 8197707511L;
+
+    case "Keeru":
+        return 6360904206L;
+
+    case "Khushi Bhuvaneshwar":
+        return 7975765492L;
+
+    case "Khushi Gmit":
+        return 8088169553L;
+
+    case "Khushi Patil":
+        return 9900480005L;
+
+    case "Khushi Pg":
+        return 9008810077L;
+
+    case "Khushi":
+        return 8296897154L;
+
+    case "Khushiiii":
+        return 8147151687L;
+
+    case "KLE Hubli":
+        return 9620107536L;
+
+    case "Krishnamurthy Anna":
+        return 6361607082L;
+
+    case "Kumar Sir":
+        return 9901904054L;
+
+    case "Lavanya Akka":
+        return 8618873823L;
+
+    case "Lavanya Cousin":
+        return 9380282019L;
+
+    case "Lavanya Schl":
+        return 9353881712L;
+
+    case "Lavanya Tamil":
+        return 6385909383L;
+
+    case "Lohit":
+        return 8618487480L;
+
+    case "Mamatha Akka":
+        return 8310640418L;
+
+    case "Manasa":
+        return 8310168109L;
+
+    case "Manjunath Sir Infosight":
+        return 9538117208L;
+
+    case "Manjunath Sir Iot":
+        return 8792049134L;
+
+    case "Manu Junio":
+        return 7022916180L;
+
+    case "Maruthi Sir":
+        return 9880099098L;
+
+    case "Medicalll":
+        return 9739844092L;
+
+    case "Meghana Akka Karate":
+        return 9902333127L;
+
+    case "Metro Electrics Bnglr":
+        return 9886559812L;
+
+    case "Mishi Amma":
+        return 9854556902L;
+
+    case "Mobile Shop":
+        return 9242434242L;
+
+    case "Moni Amma":
+        return 9482476657L;
+
+    case "Monu Akka":
+        return 8123578906L;
+
+    case "Nagaraju sir S S":
+        return 9448155167L;
+
+    case "Nandana":
+        return 8496924855L;
+
+    case "Nandish":
+        return 9945213203L;
+
+    case "Nanditha Akka":
+        return 9606776955L;
+
+    case "Nanditha Mam":
+        return 7019144210L;
+
+    case "Nandy":
+        return 8217473459L;
+
+    case "Naveen Anna Owner":
+        return 9731734499L;
+
+    case "Nayana Mam":
+        return 9945692143L;
+
+    case "Nextwave":
+        return 9538157953L;
+
+    case "Niharika Gmit":
+        return 8431080612L;
+
+    case "Nikitha Akka":
+        return 7348832046L;
+
+    case "Nikitha Gmit":
+        return 9113569969L;
+
+    case "Nikkath":
+        return 8088778749L;
+
+    case "Nischith":
+        return 9113873421L;
+
+    case "Nithya":
+        return 8217288205L;
+
+    case "Owner":
+        return 9844492128L;
+
+    case "Panda":
+        return 9611207429L;
+
+    case "Papanna":
+        return 9740375624L;
+
+    case "Pavithra":
+        return 9972212348L;
+
+    case "Pg":
+        return 8460512708L;
+
+    case "Pg Aunty":
+        return 9880969368L;
+
+    case "Pg Bnglr":
+        return 7997159670L;
+
+    case "Pooja Gmit":
+        return 8197537100L;
+
+    case "Pragathi Office":
+        return 7899884600L;
+
+    case "Pratham Gmit":
+        return 6360113636L;
+
+    case "Prathika":
+        return 8618891991L;
+
+    case "Praveen Scl":
+        return 9148855327L;
+
+    case "Preethi Akka":
+        return 8088157069L;
+
+    case "Dr Prema":
+        return 8151851423L;
+
+    case "Priyanka Gmit":
+        return 78926888810L;
+
+    case "Priyanka M":
+        return 9019447066L;
+
+    case "Priyanka M K":
+        return 9380867907L;
+
+    case "Priyanka Mam":
+        return 808815267L;
+
+    case "Rachana Mam":
+        return 6360601253L;
+
+    case "Rahul Aiml":
+        return 9108111372L;
+
+    case "Raju Uncle Hod":
+        return 9449974047L;
+
+    case "Rakshitha Is":
+        return 8618999233L;
+
+    case "Rakshitha":
+        return 963261948L;
+
+    case "Ramya":
+        return 9019113750L;
+
+    case "Ranjitha Mam":
+        return 9164066050L;
+
+    case "Ravi Anna":
+        return 9972826604L;
+
+    case "Reliance Digital":
+        return 9060382943L;
+
+    case "Rinkuu":
+        return 7795379938L;
+
+    case "Rithu Junior":
+        return 7899503663L;
+
+    case "Ruchitha":
+        return 9686208125L;
+
+    case "Sagar Gmit":
+        return 9620295446L;
+
+    case "Sahana Cs":
+        return 8956166961L;
+
+    case "Sandhya Mam":
+        return 9480687278L;
+
+    case "Sanjana Gmit":
+        return 9380407578L;
+
+    case "Sanju Mam":
+        return 8618492178L;
+
+    case "Shankar Huggi":
+        return 6364572112L;
+
+    case "Shra":
+        return 8088561194L;
+  }
+  
+  return 0;
+  }
+}
---
+++ ContactExecutor.java
@@ -0,0 +1,457 @@
+class ContactExecutor{
+    public static void main(String[] args){
+	System.out.println("main method started");
+	
+	long mummyContact = Contact.searchContactByName("Mummy");
+	System.out.println(mummyContact);
+	
+	long pappaContact = Contact.searchContactByName("Pappa");
+	System.out.println(pappaContact);
+	
+	long vivekContact = Contact.searchContactByName("Vivek Bruvv");
+	System.out.println(vivekContact);
+	
+	long mishiContact = Contact.searchContactByName("Mishii");
+	System.out.println(mishiContact);
+	
+	long dummooContact = Contact.searchContactByName("Dummoo");
+	System.out.println(dummooContact);
+	
+	long moniContact = Contact.searchContactByName("Moni");
+	System.out.println(moniContact);
+	
+	long shreyaContact = Contact.searchContactByName("Shreyyy");
+	System.out.println(shreyaContact);
+	
+	long jeevanContact = Contact.searchContactByName("Jeevan Bruh");
+	System.out.println(jeevanContact);
+	
+	long varshiContact = Contact.searchContactByName("Varshi");
+	System.out.println(varshiContact);
+	
+	long sahalContact = Contact.searchContactByName("Abnormal");
+	System.out.println(sahalContact);
+	
+	long adiContact = Contact.searchContactByName("Adi");
+	System.out.println(adiContact);
+	
+	long sisterContact = Contact.searchContactByName("Akkaa");
+	System.out.println(sisterContact);
+	
+	long aksharaContact = Contact.searchContactByName("Akshara Sir");
+	System.out.println(aksharaContact);
+	
+	long amruthaContact = Contact.searchContactByName("Amrutha");
+	System.out.println(amruthaContact);
+	
+	long ananyaContact = Contact.searchContactByName("Ananya Nuchin");
+	System.out.println(ananyaContact);
+	
+	long anuContact = Contact.searchContactByName("Anu akka");
+	System.out.println(anuContact);
+	
+	long arunContact = Contact.searchContactByName("Arun Sir");
+	System.out.println(arunContact);
+	
+	long asfiyaContact = Contact.searchContactByName("Asfiya Senio");
+	System.out.println(asfiyaContact);
+	
+	long ashaContact = Contact.searchContactByName("Asha Akka");
+	System.out.println(ashaContact);
+	
+	long avinashContact = Contact.searchContactByName("Dr.Avinash");
+	System.out.println(avinashContact);
+	
+	long banashreeContact = Contact.searchContactByName("Banashree");
+	System.out.println(banashreeContact);
+	
+	long barathContact = Contact.searchContactByName("Barath Yashu");
+	System.out.println(barathContact);
+	
+	long batrasContact = Contact.searchContactByName("Dr Batra's");
+	System.out.println(batrasContact);
+	
+	long belliContact = Contact.searchContactByName("Belli");
+	System.out.println(belliContact);
+	
+	long seniorContact = Contact.searchContactByName("Bhooma Senio");
+	System.out.println(seniorContact);
+	
+	long bhoomikaContact = Contact.searchContactByName("Bhoomika Mba");
+	System.out.println(bhoomikaContact);
+	
+	long bhumiContact = Contact.searchContactByName("Bhumi Kal");
+	System.out.println(bhumiContact);
+	
+	long xeroxContact = Contact.searchContactByName("Biet Xerox");
+	System.out.println(xeroxContact);
+	
+	long bengaluruContact = Contact.searchContactByName("Bnglr Xerox");
+	System.out.println(bengaluruContact);
+	
+	long brundhaContact = Contact.searchContactByName("Brundha");
+	System.out.println(brundhaContact);
+	
+	long chaitraContact = Contact.searchContactByName("Chaitra CS Gmit");
+	System.out.println(chaitraContact);
+	
+	long chandanaContact = Contact.searchContactByName("Chandana Xworkz");
+	System.out.println(chandanaContact);
+	
+	long chanduuContact = Contact.searchContactByName("Chanduu");
+	System.out.println(chanduuContact);
+	
+	long uncleContact = Contact.searchContactByName("Chikkappa");
+	System.out.println(uncleContact);
+	
+	long chinmayContact = Contact.searchContactByName("Chinmay Akka");
+	System.out.println(chinmayContact);
+	
+	long choreographerContact = Contact.searchContactByName("Choreographer");
+	System.out.println(choreographerContact);
+	
+	long cromaContact = Contact.searchContactByName("Croma");
+	System.out.println(cromaContact);
+	
+	long darshanContact = Contact.searchContactByName("Darshan A Sec");
+	System.out.println(darshanContact);
+	
+	long darshuContact = Contact.searchContactByName("Darshan Blore");
+	System.out.println(darshuContact);
+	
+	long deepuContact = Contact.searchContactByName("Deepu Kan");
+	System.out.println(deepuContact);
+	
+	long devuContact = Contact.searchContactByName("Devu");
+	System.out.println(devuContact);
+	
+	long dishaContact = Contact.searchContactByName("Disha A Sec");
+	System.out.println(dishaContact);
+	
+	long dodammaContact = Contact.searchContactByName("Dodamma");
+	System.out.println(dodammaContact);
+	
+	long dominosContact = Contact.searchContactByName("Dominos");
+	System.out.println(dominosContact);
+	
+	long eventContact = Contact.searchContactByName("Ekadantha");
+	System.out.println(eventContact);
+	
+	long eshwariContact = Contact.searchContactByName("Eshwari Bgscet");
+	System.out.println(eshwariContact);
+	
+	long ganeshContact = Contact.searchContactByName("Ganesh Sir");
+	System.out.println(ganeshContact);
+	
+	long meghanaContact = Contact.searchContactByName("Gmit Meghana A");
+	System.out.println(meghanaContact);
+	
+	long gubbiContact = Contact.searchContactByName("Gubbi");
+	System.out.println(gubbiContact);
+	
+	long gubbiPappaContact = Contact.searchContactByName("Gubbi Pappa");
+	System.out.println(gubbiPappaContact);
+	
+	long hamikaContact = Contact.searchContactByName("Hamika");
+	System.out.println(hamikaContact);
+	
+	long harishContact = Contact.searchContactByName("Harish Wifi");
+	System.out.println(harishContact);
+	
+	long harshaContact = Contact.searchContactByName("Harsha Cs");
+	System.out.println(harshaContact);
+	
+	long harshiContact = Contact.searchContactByName("Harshi");
+	System.out.println(harshiContact);
+	
+	long harshithaContact = Contact.searchContactByName("Harshitha Cs");
+	System.out.println(harshithaContact);
+	
+	long senioContact = Contact.searchContactByName("Harshitha Senio");
+	System.out.println(senioContact);
+	
+	long hodContact = Contact.searchContactByName("Hod");
+	System.out.println(hodContact);
+	
+	long hrContact = Contact.searchContactByName("HR");
+	System.out.println(hrContact);
+	
+	long imranContact = Contact.searchContactByName("Imran Sir");
+	System.out.println(imranContact);
+	
+	long javaContact = Contact.searchContactByName("Java Sir");
+	System.out.println(javaContact);
+	
+	long jeevContact = Contact.searchContactByName("Jeev");
+	System.out.println(jeevContact);
+	
+	long vivekkContact = Contact.searchContactByName("Jeev Vivekk");
+	System.out.println(vivekkContact);
+	
+	long brotherContact = Contact.searchContactByName("Jeevan");
+	System.out.println(brotherContact);
+	
+	long kalaContact = Contact.searchContactByName("Kala");
+	System.out.println(kalaContact);
+	
+	long kalpiContact = Contact.searchContactByName("Kalpi");
+	System.out.println(kalpiContact);
+	
+	long kanikaContact = Contact.searchContactByName("Kanika Junio");
+	System.out.println(kanikaContact);
+	
+	long kavanaContact = Contact.searchContactByName("Kavana Akka");
+	System.out.println(kavanaContact);
+	
+	long kavanaGmitContact = Contact.searchContactByName("Kavana Gmit");
+	System.out.println(kavanaGmitContact);
+	
+	long mamContact = Contact.searchContactByName("Kavya Mam");
+	System.out.println(mamContact);
+	
+	long kavyaContact = Contact.searchContactByName("Kavya N C Mam");
+	System.out.println(kavyaContact);
+	
+	long keeruContact = Contact.searchContactByName("Keeru");
+	System.out.println(keeruContact);
+	
+	long bhuvaneshwarContact = Contact.searchContactByName("Khushi Bhuvaneshwar");
+	System.out.println(bhuvaneshwarContact);
+	
+	long khushiContact = Contact.searchContactByName("Khushi Gmit");
+	System.out.println(khushiContact);
+	
+	long patilContact = Contact.searchContactByName("Khushi Patil");
+	System.out.println(patilContact);
+	
+	long pgContact = Contact.searchContactByName("Khushi Pg");
+	System.out.println(pgContact);
+	
+	long mineContact = Contact.searchContactByName("Khushi");
+	System.out.println(mineContact);
+	
+	long khushiiiiContact = Contact.searchContactByName("Khushiiii");
+	System.out.println(khushiiiiContact);
+	
+	long kleContact = Contact.searchContactByName("KLE Hubli");
+	System.out.println(kleContact);
+	
+	long krishnamurthyContact = Contact.searchContactByName("Krishnamurthy Anna");
+	System.out.println(krishnamurthyContact);
+	
+	long kumarContact = Contact.searchContactByName("Kumar Sir");
+	System.out.println(kumarContact);
+	
+	long akkaContact = Contact.searchContactByName("Lavanya Akka");
+	System.out.println(akkaContact);
+	
+	long cousinContact = Contact.searchContactByName("Lavanya Cousin");
+	System.out.println(cousinContact);
+	
+	long friendContact = Contact.searchContactByName("Lavanya Schl");
+	System.out.println(friendContact);
+	
+	long tamilContact = Contact.searchContactByName("Lavanya Tamil");
+	System.out.println(tamilContact);
+	
+	long lohitContact = Contact.searchContactByName("Lohit");
+	System.out.println(lohitContact);
+	
+	long mamathaContact = Contact.searchContactByName("Mamatha Akka");
+	System.out.println(mamathaContact);
+	
+	long manasaContact = Contact.searchContactByName("Manasa");
+	System.out.println(manasaContact);
+	
+	long infosightContact = Contact.searchContactByName("Manjunath Sir Infosight");
+	System.out.println(infosightContact);
+	
+	long manjunathContact = Contact.searchContactByName("Manjunath Sir Iot");
+	System.out.println(manjunathContact);
+	
+	long manuContact = Contact.searchContactByName("Manu Junio");
+	System.out.println(manuContact);
+	
+	long maruthiContact = Contact.searchContactByName("Maruthi Sir");
+	System.out.println(maruthiContact);
+	
+	long medicalContact = Contact.searchContactByName("Medicalll");
+	System.out.println(medicalContact);
+	
+	long meghanaKarateContact = Contact.searchContactByName("Meghana Akka Karate");
+	System.out.println(meghanaKarateContact);
+	
+	long metroContact = Contact.searchContactByName("Metro Electrics Bnglr");
+	System.out.println(metroContact);
+	
+	long mishiAmmaContact = Contact.searchContactByName("Mishi Amma");
+	System.out.println(mishiAmmaContact);
+	
+	long mobileShopContact = Contact.searchContactByName("Mobile Shop");
+	System.out.println(mobileShopContact);
+	
+	long moniAmmaContact = Contact.searchContactByName("Moni Amma");
+	System.out.println(moniAmmaContact);
+	
+	long monuContact = Contact.searchContactByName("Monu Akka");
+	System.out.println(monuContact);
+	
+	long nagarajuContact = Contact.searchContactByName("Nagaraju sir S S");
+	System.out.println(nagarajuContact);
+	
+	long nandanaContact = Contact.searchContactByName("Nandana");
+	System.out.println(nandanaContact);
+	
+	long nandishContact = Contact.searchContactByName("Nandish");
+	System.out.println(nandishContact);
+	
+	long nandithaContact = Contact.searchContactByName("Nanditha Akka");
+	System.out.println(nandithaContact);
+	
+	long nandithaMamContact = Contact.searchContactByName("Nanditha Mam");
+	System.out.println(nandithaMamContact);
+	
+	long nandyContact = Contact.searchContactByName("Nandy");
+	System.out.println(nandyContact);
+	
+	long naveenContact = Contact.searchContactByName("Naveen Anna Owner");
+	System.out.println(naveenContact);
+	
+	long nayanaContact = Contact.searchContactByName("Nayana Mam");
+	System.out.println(nayanaContact);
+	
+	long nextwaveContact = Contact.searchContactByName("Nextwave");
+	System.out.println(nextwaveContact);
+	
+	long niharikaContact = Contact.searchContactByName("Niharika Gmit");
+	System.out.println(niharikaContact);
+	
+	long nikithaContact = Contact.searchContactByName("Nikitha Akka");
+	System.out.println(nikithaContact);
+	
+	long nikithaGmitContact = Contact.searchContactByName("Nikitha Gmit");
+	System.out.println(nikithaGmitContact);
+	
+	long nikkathContact = Contact.searchContactByName("Nikkath");
+	System.out.println(nikkathContact);
+	
+	long nischithContact = Contact.searchContactByName("Nischith");
+	System.out.println(nischithContact);
+	
+	long nithyaContact = Contact.searchContactByName("Nithya");
+	System.out.println(nithyaContact);
+	
+	long ownerContact = Contact.searchContactByName("Owner");
+	System.out.println(ownerContact);
+	
+	long pandaContact = Contact.searchContactByName("Panda");
+	System.out.println(pandaContact);
+	
+	long papannaContact = Contact.searchContactByName("Papanna");
+	System.out.println(papannaContact);
+	
+	long pavitraContact = Contact.searchContactByName("Pavithra");
+	System.out.println(pavitraContact);
+	
+	long payingGuestContact = Contact.searchContactByName("Pg");
+	System.out.println(payingGuestContact);
+	
+	long pgAuntyContact = Contact.searchContactByName("Pg Aunty");
+	System.out.println(pgAuntyContact);
+	
+	long pgBengaluruContact = Contact.searchContactByName("Pg Bnglr");
+	System.out.println(pgBengaluruContact);
+	
+	long poojaContact = Contact.searchContactByName("Pooja Gmit");
+	System.out.println(poojaContact);
+	
+	long pragathiContact = Contact.searchContactByName("Pragathi Office");
+	System.out.println(pragathiContact);
+	
+	long prathamContact = Contact.searchContactByName("Pratham Gmit");
+	System.out.println(prathamContact);
+	
+	long prathikaContact = Contact.searchContactByName("Prathika");
+	System.out.println(prathikaContact);
+	
+	long praveenContact = Contact.searchContactByName("Praveen Scl");
+	System.out.println(praveenContact);
+	
+	long preethiContact = Contact.searchContactByName("Preethi Akka");
+	System.out.println(preethiContact);
+	
+	long premaContact = Contact.searchContactByName("Dr Prema");
+	System.out.println(premaContact);
+	
+	long priyankaContact = Contact.searchContactByName("Priyanka Gmit");
+	System.out.println(priyankaContact);
+	
+	long priyankaMContact = Contact.searchContactByName("Priyanka M");
+	System.out.println(priyankaMContact);
+	
+	long priyankaMKContact = Contact.searchContactByName("Priyanka M K");
+	System.out.println(priyankaMKContact);
+	
+	long priyankaMamContact = Contact.searchContactByName("Priyanka Mam");
+	System.out.println(priyankaMamContact);
+	
+	long rachanaContact = Contact.searchContactByName("Rachana Mam");
+	System.out.println(rachanaContact);
+	
+	long rahulContact = Contact.searchContactByName("Rahul Aiml");
+	System.out.println(rahulContact);
+	
+	long rajuContact = Contact.searchContactByName("Raju Uncle Hod");
+	System.out.println(rajuContact);
+	
+	long rakshithaISContact = Contact.searchContactByName("Rakshitha Is");
+	System.out.println(rakshithaISContact);
+	
+	long rakshithaContact = Contact.searchContactByName("Rakshitha");
+	System.out.println(rakshithaContact);
+	
+	long ramyaContact = Contact.searchContactByName("Ramya");
+	System.out.println(ramyaContact);
+	
+	long ranjithaContact = Contact.searchContactByName("Ranjitha Mam");
+	System.out.println(ranjithaContact);
+	
+	long raviContact = Contact.searchContactByName("Ravi Anna");
+	System.out.println(raviContact);
+	
+	long relianceDigitalContact = Contact.searchContactByName("Reliance Digital");
+	System.out.println(relianceDigitalContact);
+	
+	long rinkuContact = Contact.searchContactByName("Rinkuu");
+	System.out.println(rinkuContact);
+	
+	long rithuContact = Contact.searchContactByName("Rithu Junior");
+	System.out.println(rithuContact);
+	
+	long ruchithaContact = Contact.searchContactByName("Ruchitha");
+	System.out.println(ruchithaContact);
+	
+	long sagarContact = Contact.searchContactByName("Sagar Gmit");
+	System.out.println(sagarContact);
+	
+	long sahanaContact = Contact.searchContactByName("Sahana Cs");
+	System.out.println(sahanaContact);
+	
+	long sandhyaMamContact = Contact.searchContactByName("Sandhya Mam");
+	System.out.println(sandhyaMamContact);
+	
+	long sanjanaContact = Contact.searchContactByName("Sanjana Gmit");
+	System.out.println(sanjanaContact);
+	
+	long sanjuMamContact = Contact.searchContactByName("Sanju Mam");
+	System.out.println(sanjuMamContact);
+	
+	long huggiContact = Contact.searchContactByName("Shankar Huggi");
+	System.out.println(huggiContact);
+	
+	long shraddhaContact = Contact.searchContactByName("Shra");
+	System.out.println(shraddhaContact);
+	
+	System.out.println("main method ended");
+	}
+}
